ee6dc8389ee16596def7ad9e3f2b836e21176cf0
Jessica <jessicahuangtfg@gmail.com>
2018-11-02 22:00:33 +0800

    Add seamentic

--- views/main.js
+++ views/main.js
@@ -1,4 +1,5 @@
-require('bootstrap/dist/css/bootstrap.min.css');
+//require('bootstrap/dist/css/bootstrap.min.css');
+
 import '../public/css/quill.snow.css';
 import Vue from 'vue'
 import App from './App.vue'
